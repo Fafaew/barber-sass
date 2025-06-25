c6607e090c9f114c95bfe6a2714e2282a0e43840
Rafael <rafael.gomest@gmail.com>
2025-06-25 18:58:00 -0300

    added header, search input and public files

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,35 @@
-"use client"
-
+import { Input } from "@/components/ui/input"
+import Header from "./_components/header"
 import React from "react"
 import { Button } from "./_components/ui/button"
+import { SearchIcon } from "lucide-react"
+import Image from "next/image"
 
 const Home = () => {
-  return <Button>Home</Button>
+  return (
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Rafael</h2>
+        <p>Sexta-feira, 25 de junho.</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            alt="barber-image"
+            src="/banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Home
