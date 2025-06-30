d66fa5f5fb63f98e4ec325f97b9404150d485b0c
Rafael <rafael.gomest@gmail.com>
2025-06-30 19:02:25 -0300

    booking item

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import { SearchIcon } from "lucide-react"
 import Image from "next/image"
 import { Card, CardContent } from "@/components/ui/card"
 import { Badge } from "@/components/ui/badge"
+import { Avatar, AvatarImage } from "@/components/ui/avatar"
 
 const Home = () => {
   return (
@@ -31,11 +32,21 @@ const Home = () => {
         </div>
         <div>
           {/* agendamento */}
-          <Card className="mt-6">
+          <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+            Agendamentos
+          </h2>
+          <Card>
             <CardContent className="flex justify-between p-0">
               {/* ESQUERDA */}
               <div className="flex flex-col gap-2 py-5 pl-5">
-                <Badge>Confirmado</Badge>
+                <Badge className="w-fit">Confirmado</Badge>
+                <h3 className="font-semibold">Corte de Cabelo</h3>
+                <div className="flex items-center gap-2">
+                  <Avatar className="h-6 w-6">
+                    <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
+                  </Avatar>
+                  <p className="text-sm">barbearia FSW</p>
+                </div>
               </div>
               {/* DIREITA */}
               <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
