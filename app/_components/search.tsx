dfe176fa0e0f5a9c4da8ba9bc0853c20cfe9b46c
Rafael <rafael.gomest@gmail.com>
2025-07-16 20:42:59 -0300

    added search logic and quick search buttons logic

--- app/_components/search.tsx
+++ app/_components/search.tsx
@@ -16,7 +16,7 @@ import {
 } from "@/components/ui/form"
 
 const formSchema = z.object({
-  search: z.string().trim().min(1, {
+  title: z.string().trim().min(1, {
     message: "Digite algo para buscar",
   }),
 })
@@ -25,14 +25,14 @@ const Search = () => {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      search: "",
+      title: "",
     },
   })
 
   const router = useRouter()
 
   const handleSubmit = (data: z.infer<typeof formSchema>) => {
-    router.push(`/barbershops?search=${data.search}`)
+    router.push(`/barbershops?title=${data.title}`)
   }
 
   return (
@@ -40,7 +40,7 @@ const Search = () => {
       <form onSubmit={form.handleSubmit(handleSubmit)} className="flex gap-2">
         <FormField
           control={form.control}
-          name="search"
+          name="title"
           render={({ field }) => (
             <FormItem className="w-full">
               <FormControl>
